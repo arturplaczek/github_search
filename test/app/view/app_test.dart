af08fd7971f7636a1af34c40354f9388354f885e
arturplaczek <arturp1989@gmail.com>
2022-12-31 14:43:42 +0100

    fix: tests

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,11 +1,24 @@
 import 'package:api_client/api_client.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:github_search/app/app.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mocktail/mocktail.dart';
 
 class _MockApiClient extends Mock implements ApiClient {}
 
+class _MockStorage extends Mock implements Storage {}
+
 void main() {
+  late Storage storage;
+
+  setUp(() {
+    storage = _MockStorage();
+    when(
+      () => storage.write(any(), any<dynamic>()),
+    ).thenAnswer((_) async {});
+    HydratedBloc.storage = storage;
+  });
+
   group('App', () {
     testWidgets('renders AppView', (WidgetTester tester) async {
       await tester.pumpWidget(
