db47f912c0178c1e74bc9a273b725e562d58366b
arturplaczek <arturp1989@gmail.com>
2023-01-04 20:16:39 +0100

    feat: add share

--- test/home/view/home_page_test.dart
+++ test/home/view/home_page_test.dart
@@ -1,10 +1,13 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:github_repository/github_repository.dart';
 import 'package:github_search/home/home.dart';
 import 'package:github_search/l10n/l10n.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:share_plus_platform_interface/method_channel/method_channel_share.dart';
+import 'package:share_plus_platform_interface/share_plus_platform_interface.dart';
 
 import '../../helpers.dart';
 
@@ -12,19 +15,34 @@ class _MockGithubSearchBloc
     extends MockBloc<GithubSearchEvent, GithubSearchState>
     implements GithubSearchBloc {}
 
+class _MockMethodChannel extends Mock implements MethodChannel {}
+
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   late GithubSearchBloc bloc;
+  late SharePlatform sharePlatform;
+  late _MockMethodChannel mockChannel;
 
   setUp(() {
     bloc = _MockGithubSearchBloc();
+    sharePlatform = SharePlatform();
+
     when(() => bloc.state).thenReturn(const GithubSearchState());
     when(() => bloc.stream).thenAnswer(
       (_) => Stream.value(const GithubSearchState()),
     );
+
+    mockChannel = _MockMethodChannel();
+    TestDefaultBinaryMessengerBinding.instance!.defaultBinaryMessenger
+        .setMockMethodCallHandler(MethodChannelShare.channel, (call) async {
+      await mockChannel.invokeMethod<void>(call.method, call.arguments);
+      return Future(() => null);
+    });
   });
 
   group('HomePage', () {
-    testWidgets('renders HomeView', (WidgetTester tester) async {
+    testWidgets('renders HomeView', (tester) async {
       await tester.pumpSubject(
         const HomePage(),
       );
@@ -34,7 +52,7 @@ void main() {
   });
 
   group('HomeView', () {
-    testWidgets('renders GithubNameTextfield', (WidgetTester tester) async {
+    testWidgets('renders GithubNameTextfield', (tester) async {
       await tester.pumpSubject(
         const HomePage(),
       );
@@ -63,7 +81,7 @@ void main() {
 
   group('HomeViewBody', () {
     testWidgets('renders CircularProgressIndicator on loading state',
-        (WidgetTester tester) async {
+        (tester) async {
       const state = GithubSearchState(
         status: GithubSearchStatus.loading,
       );
@@ -82,8 +100,7 @@ void main() {
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
     });
 
-    testWidgets('renders error text on failure state',
-        (WidgetTester tester) async {
+    testWidgets('renders error text on failure state', (tester) async {
       const state = GithubSearchState(
         status: GithubSearchStatus.failure,
       );
@@ -104,8 +121,7 @@ void main() {
       expect(find.text(l10n.errorText), findsOneWidget);
     });
 
-    testWidgets('renders CommitList on success state',
-        (WidgetTester tester) async {
+    testWidgets('renders CommitList on success state', (tester) async {
       final state = GithubSearchState(
         repository: GithubRepositoryModel(
           id: -1,
@@ -134,6 +150,106 @@ void main() {
 
       expect(find.byType(CommitList), findsOneWidget);
       expect(find.byType(CommitWidget), findsOneWidget);
+    });
+  });
+
+  group('ShareButton', () {
+    testWidgets('renders SizedBox wihout selected commit', (tester) async {
+      final state = GithubSearchState(
+        repository: GithubRepositoryModel(
+          id: -1,
+          commits: [
+            GithubCommitModel(
+              sha: 'sha',
+              message: 'message',
+              authorName: 'authorName',
+              date: DateTime(2022),
+            ),
+          ],
+        ),
+        status: GithubSearchStatus.success,
+      );
+      when(() => bloc.state).thenReturn(state);
+      when(() => bloc.stream).thenAnswer(
+        (_) => Stream.value(state),
+      );
+
+      await tester.pumpSubject(
+        const ShareButton(),
+        githubSearchBloc: bloc,
+      );
+
+      expect(find.byType(SizedBox), findsOneWidget);
+      expect(find.byType(TextButton), findsNothing);
+    });
+
+    testWidgets('renders TextButton with selected commit', (tester) async {
+      final state = GithubSearchState(
+        repository: GithubRepositoryModel(
+          id: -1,
+          commits: [
+            GithubCommitModel(
+              sha: 'sha',
+              message: 'message',
+              authorName: 'authorName',
+              date: DateTime(2022),
+              isSelected: true,
+            ),
+          ],
+        ),
+        status: GithubSearchStatus.success,
+      );
+      when(() => bloc.state).thenReturn(state);
+      when(() => bloc.stream).thenAnswer(
+        (_) => Stream.value(state),
+      );
+
+      await tester.pumpSubject(
+        const ShareButton(),
+        githubSearchBloc: bloc,
+      );
+
+      expect(find.byType(TextButton), findsOneWidget);
+      expect(find.byType(SizedBox), findsNothing);
+    });
+
+    testWidgets('share on preseed', (tester) async {
+      final state = GithubSearchState(
+        repository: GithubRepositoryModel(
+          id: -1,
+          commits: [
+            GithubCommitModel(
+              sha: 'sha',
+              message: 'message',
+              authorName: 'authorName',
+              date: DateTime(2022),
+              isSelected: true,
+            ),
+          ],
+        ),
+        status: GithubSearchStatus.success,
+      );
+      when(() => bloc.state).thenReturn(state);
+      when(() => bloc.stream).thenAnswer(
+        (_) => Stream.value(state),
+      );
+
+      await tester.pumpSubject(
+        const ShareButton(),
+        githubSearchBloc: bloc,
+      );
+
+      expect(find.byType(TextButton), findsOneWidget);
+
+      await tester.tap(find.byType(TextButton));
+
+      // TODO: fix this test
+      // verify(() => sharePlatform.share(any())).called(1);
+      // verify(
+      //   () => mockChannel.invokeMethod<void>('share', <String, dynamic>{
+      //     'text': state.getSelectedCommitsMessage(),
+      //   }),
+      // );
     });
   });
 }
