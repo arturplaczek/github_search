27250bd78f72975587b7ed8a15807a5d83b15924
Artur Płaczek <arturplaczek@gmail.com>
2024-01-22 17:24:28 +0100

    chore: add AppBlocObserver to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'dart:developer';
+
 import 'package:api_client/api_client.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 import 'package:github_search/app/app.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+
+class AppBlocObserver extends BlocObserver {
+  AppBlocObserver() : super();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    log('onChange(${bloc.runtimeType}, $change)');
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    super.onError(bloc, error, stackTrace);
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,6 +31,8 @@ void main() async {
         : await getTemporaryDirectory(),
   );
   final apiClient = ApiClient();
+
+  Bloc.observer = AppBlocObserver();
 
   runApp(
     App(
